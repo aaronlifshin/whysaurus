ce6e1e9e3131ed11e487c0ee9f944e1555dbb11d
Colin Curtin <colin.t.curtin@gmail.com>
2013-04-07 20:47:44 -0700

    autopep8

--- WhySaurus.py
+++ WhySaurus.py
@@ -19,18 +19,19 @@ from WhySaurusModels import WhysaurusUser
 
 from simpleauth import SimpleAuthHandler
 
-class WhysaurusRequestHandler(webapp2.RequestHandler):
-  def dispatch(self):
-    # Get a session store for this request.
-    self.session_store = sessions.get_store(request=self.request)
 
-    try:
-      # Dispatch the request.
-      webapp2.RequestHandler.dispatch(self)      
-    finally:
-      # Save all sessions.
-      self.session_store.save_sessions(self.response)
-         
+class WhysaurusRequestHandler(webapp2.RequestHandler):
+    def dispatch(self):
+        # Get a session store for this request.
+        self.session_store = sessions.get_store(request=self.request)
+
+        try:
+            # Dispatch the request.
+            webapp2.RequestHandler.dispatch(self)
+        finally:
+            # Save all sessions.
+            self.session_store.save_sessions(self.response)
+
   @webapp2.cached_property
   def session(self):
     """Returns a session using the default cookie key"""
@@ -53,7 +54,7 @@ class WhysaurusRequestHandler(webapp2.RequestHandler):
   def logged_in(self):
     """Returns true if a user is currently logged in, false otherwise"""
     return self.auth.get_user_by_session() is not None
-    
+
   def render(self, template_name, template_vars={}):
     # Preset values for the template
     values = {
@@ -73,7 +74,7 @@ class WhysaurusRequestHandler(webapp2.RequestHandler):
 
   def head(self, *args):
     """Head is used by Twitter. If not there the tweet button shows 0"""
-    pass     
+    pass
 
 
 class AuthHandler(WhysaurusRequestHandler, SimpleAuthHandler):
@@ -85,7 +86,7 @@ class AuthHandler(WhysaurusRequestHandler, SimpleAuthHandler):
 
   USER_ATTRS = {
     'facebook' : {
-      'id'     : lambda id: ('avatar_url', 
+      'id'     : lambda id: ('avatar_url',
         'http://graph.facebook.com/{0}/picture?type=square'.format(id)),
       'name'   : 'name',
       'link'   : 'link',
@@ -118,13 +119,13 @@ class AuthHandler(WhysaurusRequestHandler, SimpleAuthHandler):
       'email'   : 'link'
     }
   }
-  
-  
+
+
 
   def _on_signin(self, data, auth_info, provider):
     auth_id = '%s:%s' % (provider, data['id'])
     logging.info('Looking for a user with id %s', auth_id)
-    
+
     user = self.auth.store.user_model.get_by_auth_id(auth_id)
     _attrs = self._to_user_model_attrs(data, self.USER_ATTRS[provider])
     if user:
@@ -138,15 +139,15 @@ class AuthHandler(WhysaurusRequestHandler, SimpleAuthHandler):
       user.populate(**_attrs)
       user.put()
       self.auth.set_session( self.auth.store.user_to_dict(user))
-      
+
     else:
       # check whether there's a user currently logged in
-      # then, create a new user if nobody's signed in, 
+      # then, create a new user if nobody's signed in,
       # otherwise add this auth_id to currently logged in user.
 
       if self.logged_in and self.current_user:
         logging.info('Updating currently logged in user')
-        
+
         u = self.current_user
         u.populate(**_attrs)
         # The following will also do u.put(). Though, in a real app
@@ -154,7 +155,7 @@ class AuthHandler(WhysaurusRequestHandler, SimpleAuthHandler):
         # (boolean, info) tuple where boolean == True indicates success
         # See webapp2_extras.appengine.auth.models.User for details.
         u.add_auth_id(auth_id)
-        
+
       else:
         logging.info('Creating a brand new user. Auth_id: %s ', str(auth_id))
         ok, user = self.auth.store.user_model.create_user(auth_id, **_attrs)
@@ -170,7 +171,7 @@ class AuthHandler(WhysaurusRequestHandler, SimpleAuthHandler):
     # Go to the profile page
     target = str(self.session['original_url'])
     self.redirect(target)
-    
+
   def logout(self):
     self.auth.unset_session()
     self.redirect('/')
@@ -182,34 +183,34 @@ class AuthHandler(WhysaurusRequestHandler, SimpleAuthHandler):
     """Should return a tuple (key, secret) for auth init requests.
     For OAuth 2.0 you should also return a scope, e.g.
     ('my app id', 'my app secret', 'email,user_about_me')
-    
+
     The scope depends solely on the provider.
     """
     return constants.AUTH_CONFIG[provider]
-    
+
   def _to_user_model_attrs(self, data, attrs_map):
     """Get the needed information from the provider dataset."""
     user_attrs = {}
     for k, v in attrs_map.iteritems():
       attr = (v, data.get(k)) if isinstance(v, str) else v(data.get(k))
-      user_attrs.setdefault(*attr)    
+      user_attrs.setdefault(*attr)
     return user_attrs
-            
+
 class MainPage(AuthHandler):
-  def get(self): 
-        
+  def get(self):
+
     newPoints = PointRoot.getRecentCurrentPoints()
     user = None
-    
+
     if self.logged_in:
       user = self.current_user
-  
+
     # GET RECENTLY VIEWED
     if user:
       recentlyViewedPoints = user.getRecentlyViewed()
     else:
       recentlyViewedPoints = []
-  
+
     editorsPicksPoints = PointRoot.getEditorsPicks()
     topPoints = PointRoot.getTopRatedPoints()
     template_values = {
@@ -225,24 +226,24 @@ class MainPage(AuthHandler):
 
 
 class NewPoint(AuthHandler):
-  def post(self):
-    user = self.current_user
-    resultJSON = json.dumps({'result':False, 'error':'Not authorized'})
-    if user:
-      newPoint, newPointRoot = Point.create(
-        title=self.request.get('title'),
-        content=self.request.get('content'), 
-        summaryText=self.request.get('plainText'),
-        user=user,
-        imageURL=self.request.get('imageURL'),
-        imageAuthor=self.request.get('imageAuthor'),
-        imageDescription=self.request.get('imageDescription'))
-      if newPoint:
-        resultJSON = json.dumps({'result':True, 'pointURL':newPoint.url })
-      else:
-        resultJSON = json.dumps({'result':False, 'error':'Failed to create point.' })
-    self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
-    self.response.out.write(resultJSON)
+    def post(self):
+        user = self.current_user
+        resultJSON = json.dumps({'result': False, 'error': 'Not authorized'})
+        if user:
+            newPoint, newPointRoot = Point.create(
+                title=self.request.get('title'),
+                content=self.request.get('content'),
+                summaryText=self.request.get('plainText'),
+                user=user,
+                imageURL=self.request.get('imageURL'),
+                imageAuthor=self.request.get('imageAuthor'),
+                imageDescription=self.request.get('imageDescription'))
+        if newPoint:
+            resultJSON = json.dumps({'result': True, 'pointURL': newPoint.url})
+        else:
+            resultJSON = json.dumps({'result': False, 'error': 'Failed to create point.'})
+        self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
+        self.response.out.write(resultJSON)
 
 
 class DeletePoint(AuthHandler):
@@ -253,7 +254,7 @@ class DeletePoint(AuthHandler):
       resultJSON = json.dumps({'result':False, 'error':'Need to be logged in'})
     elif not user.admin:
       resultJSON = json.dumps({'result':False, 'error':'Must be admin'})
-    else:   
+    else:
       urlToDelete = self.request.get('urlToDelete');
       point, pointRoot = Point.getCurrentByUrl(urlToDelete)
 
@@ -275,12 +276,12 @@ class ViewPoint(AuthHandler):
     if point:
       supportingPoints = point.getSupportingPoints()
       user = self.current_user
-      if not user or not user.userVotes or not point.key.parent() in user.userVotes: 
+      if not user or not user.userVotes or not point.key.parent() in user.userVotes:
         voteValue = 0
       else:
         voteValue = user.userVotes[point.key.parent()].value
 
-      addedToRecentlyViewed = False      
+      addedToRecentlyViewed = False
       if user:
         addedToRecentlyViewed = user.updateRecentlyViewed(point.key.parent())
 
@@ -305,30 +306,30 @@ class ViewPoint(AuthHandler):
 
 # AJAX. CALLED FROM THE POINT VIEW PAGE
 class EditPoint(AuthHandler):
-  def post(self): 
+  def post(self):
     resultJSON = json.dumps({'result':False})
     oldPoint, oldPointRoot = Point.getCurrentByUrl(self.request.get('urlToEdit'))
     newVersion = oldPoint.update(
-      newTitle=self.request.get('title'), 
+      newTitle=self.request.get('title'),
       newContent=self.request.get('content'),
       newSummaryText=self.request.get('plainText'),
       user=self.current_user,
       imageURL=self.request.get('imageURL'),
       imageAuthor=self.request.get('imageAuthor'),
-      imageDescription=self.request.get('imageDescription') 
+      imageDescription=self.request.get('imageDescription')
       )
     if newVersion:
-      resultJSON = json.dumps({'result':True, 
+      resultJSON = json.dumps({'result':True,
         'version':newVersion.version,
         'author':newVersion.authorName,
         'dateEdited': str(newVersion.dateEdited),
         'imageURL': newVersion.imageURL,
         'imageAuthor': newVersion.imageAuthor,
-        'imageDescription': newVersion.imageDescription 
+        'imageDescription': newVersion.imageDescription
         })
     self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
     self.response.out.write(resultJSON)
-    
+
 class UnlinkPoint(AuthHandler):
   def post(self):
     resultJSON = json.dumps({'result':False})
@@ -341,15 +342,15 @@ class UnlinkPoint(AuthHandler):
           resultJSON = json.dumps({'result':True, 'pointURL':supportingPointURL})
     self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
     self.response.out.write(resultJSON)
-    
+
 class LinkPoint(AuthHandler):
   def post(self):
     resultJSON = json.dumps({'result':False})
     supportingPoint, supportingPointRoot = Point.getCurrentByUrl(self.request.get('supportingPointURL'))
-    oldPoint, oldPointRoot = Point.getCurrentByUrl(self.request.get('parentPointURL'))   
+    oldPoint, oldPointRoot = Point.getCurrentByUrl(self.request.get('parentPointURL'))
     user = self.current_user
-    
-    if user:      
+
+    if user:
       try:
         newPoint = oldPoint.update(
           newSupportingPoint=supportingPointRoot,
@@ -357,12 +358,12 @@ class LinkPoint(AuthHandler):
           )
       except WhysaurusException as e:
         resultJSON = json.dumps({'result':False, 'error': str(e)})
-      else:   
+      else:
         resultJSON = json.dumps({'result':True})
     else:
       resultJSON = json.dumps({'result':'ACCESS DENIED!'})
     self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
-    self.response.out.write(resultJSON)   
+    self.response.out.write(resultJSON)
 
 class SelectSupportingPoint(AuthHandler):
   def post(self):
@@ -371,8 +372,8 @@ class SelectSupportingPoint(AuthHandler):
     if user:
       oldPoint, oldPointRoot = Point.getCurrentByUrl(self.request.get('parentPointURL'))
       recentlyViewedPoints = user.getRecentlyViewed(
-                                                    excludeList = 
-                                                    [oldPoint.key.parent()] + oldPoint.supportingPoints 
+                                                    excludeList =
+                                                    [oldPoint.key.parent()] + oldPoint.supportingPoints
                                                      )
     else:
       recentlyViewedPoints = []
@@ -389,7 +390,7 @@ class SelectSupportingPoint(AuthHandler):
 class PointHistory(AuthHandler):
   def get(self):
     pointData = Point.getFullHistory(self.request.get('pointUrl'))
-    
+
     template_values = {
       'latestPoint' : pointData[0]["point"] if pointData else None,
       'numPoints': len(pointData) if pointData else 0,
@@ -406,18 +407,18 @@ class AddSupportingPoint(AuthHandler):
     jsonOutput = {'result':False}
     oldPoint, oldPointRoot = Point.getCurrentByUrl(self.request.get('pointUrl'))
     user = self.current_user
-    
+
     if user:
       supportingPoint, supportingPointRoot = Point.create(
-        title=self.request.get('title'), 
+        title=self.request.get('title'),
         content=self.request.get('content'),
         summaryText=self.request.get('plainText'),
-        user=user,        
+        user=user,
         pointSupported=oldPoint.key.parent(),
         imageURL=self.request.get('imageURL'),
         imageAuthor=self.request.get('imageAuthor'),
-        imageDescription=self.request.get('imageDescription'))     
-      try: 
+        imageDescription=self.request.get('imageDescription'))
+      try:
         newPoint = oldPoint.update(
           newSupportingPoint=supportingPointRoot,
           user=user
@@ -427,7 +428,7 @@ class AddSupportingPoint(AuthHandler):
           'result':False,
           'err':str(e)
         }
-      else: 
+      else:
         jsonOutput = {
           'result':True,
           'version':newPoint.version,
@@ -436,7 +437,7 @@ class AddSupportingPoint(AuthHandler):
         }
       resultJSON = json.dumps(jsonOutput)
       self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
-      self.response.out.write(resultJSON)  
+      self.response.out.write(resultJSON)
     else:
       self.response.out.write('Need to be logged in')
 
@@ -451,7 +452,7 @@ class Vote(AuthHandler):
         resultJSON = json.dumps({'result':True, 'newVote':self.request.get('vote')})
     self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
     self.response.out.write(resultJSON)
- 
+
 class Search(AuthHandler):
   def get(self):
     searchResults = Point.search(self.request.get('searchTerms'))
@@ -475,8 +476,8 @@ class AjaxSearch(AuthHandler):
         'searchString': self.request.get('searchTerms')
       })
     self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
-    self.response.out.write(resultJSON)   
-    
+    self.response.out.write(resultJSON)
+
 class TestPage(AuthHandler):
   def get(self):
     user = self.current_user
@@ -487,80 +488,77 @@ class TestPage(AuthHandler):
     path = os.path.join(os.path.dirname(__file__), 'templates/test.html')
     self.response.out.write(template.render(path, template_values))
 
-    
+
 class About(AuthHandler):
   def get(self):
     path = os.path.join(os.path.dirname(__file__), 'templates/about.html')
-    self.response.out.write(template.render(path, {'user' : self.current_user}))   
-        
+    self.response.out.write(template.render(path, {'user' : self.current_user}))
+
 class Help(AuthHandler):
   def get(self):
     path = os.path.join(os.path.dirname(__file__), 'templates/help.html')
-    self.response.out.write(template.render(path, {'user' : self.current_user}))     
-        
+    self.response.out.write(template.render(path, {'user' : self.current_user}))
+
 class Contact(AuthHandler):
   def get(self):
     path = os.path.join(os.path.dirname(__file__), 'templates/contact.html')
-    self.response.out.write(template.render(path, {'user' : self.current_user}))   
+    self.response.out.write(template.render(path, {'user' : self.current_user}))
 
 class ContactSend(AuthHandler):
-  def post(self):
-    mail.send_mail(sender='aaron@whysaurus.com',
-       to= 'aaronlifshin@gmail.com',
-       subject= 'WHYSAURUS CONTACT RE:' + self.request.get('subject'), 
-       body= 'From: ' + self.request.get('name') + '\n' + self.request.get('message'),
-       reply_to=self.request.get('email')
-    )
-    
-    template_values = {
-      'message' : "Thank you for your message.",
-    }
-    path = os.path.join(os.path.dirname(__file__), 'templates/message.html')
-    self.response.out.write(template.render(path, template_values))
-    
-    
+    def post(self):
+        mail.send_mail(sender = 'aaron@whysaurus.com',
+                       to = 'aaronlifshin@gmail.com',
+                       subject = 'WHYSAURUS CONTACT RE:' + self.request.get('subject'),
+                       body = 'From: ' + self.request.get('name') + '\n' + self.request.get('message'),
+                       reply_to = self.request.get('email')
+        )
+
+        template_values = {
+            'message': "Thank you for your message.",
+        }
+        path = os.path.join(os.path.dirname(__file__), 'templates/message.html')
+        self.response.out.write(template.render(path, template_values))
+
+
 # Map URLs to handlers
 routes = [
-	Route('/', MainPage),
-  Route('/about', About),
-  Route('/help', Help),
-  Route('/contact', Contact),
-  Route('/contactSend', ContactSend),
-  Route('/newPoint', NewPoint),
-  Route('/deletePoint', DeletePoint),
-	Route('/editPoint', EditPoint),
-	Route('/unlinkPoint', UnlinkPoint),
-	Route('/point/<pointURL>', ViewPoint),
-	Route('/addSupportingPoint', AddSupportingPoint),
-	Route('/selectSupportingPoint', SelectSupportingPoint),
-  Route('/linkPoint', LinkPoint),	
-	Route('/vote', Vote),
-	Route('/testPage', TestPage),
-  Route('/search', Search),
-  Route('/ajaxSearch', AjaxSearch),
-	Route('/pointHistory',PointHistory), 
-  # Route('/profile', handler='handlers.ProfileHandler', name='profile'),
-  Route('/logout', handler='WhySaurus.AuthHandler:logout', name='logout'), # , handler_method='logout', name='logout'),
-  Route('/auth/<provider>', handler='WhySaurus.AuthHandler:_simple_auth', name='auth_login') , 
-  Route('/auth/<provider>/callback', handler='WhySaurus.AuthHandler:_auth_callback', name='auth_callback')
+    Route('/', MainPage),
+    Route('/about', About),
+    Route('/help', Help),
+    Route('/contact', Contact),
+    Route('/contactSend', ContactSend),
+    Route('/newPoint', NewPoint),
+    Route('/deletePoint', DeletePoint),
+    Route('/editPoint', EditPoint),
+    Route('/unlinkPoint', UnlinkPoint),
+    Route('/point/<pointURL>', ViewPoint),
+    Route('/addSupportingPoint', AddSupportingPoint),
+    Route('/selectSupportingPoint', SelectSupportingPoint),
+    Route('/linkPoint', LinkPoint),
+    Route('/vote', Vote),
+    Route('/testPage', TestPage),
+    Route('/search', Search),
+    Route('/ajaxSearch', AjaxSearch),
+    Route('/pointHistory', PointHistory),
+    # Route('/profile', handler='handlers.ProfileHandler', name='profile'),
+    Route('/logout', handler='WhySaurus.AuthHandler:logout', name='logout'),  # , handler_method='logout', name='logout'),
+    Route('/auth/<provider>', handler='WhySaurus.AuthHandler:_simple_auth', name='auth_login'),
+    Route('/auth/<provider>/callback', handler='WhySaurus.AuthHandler:_auth_callback', name='auth_callback')
 ]
-  
+
 # webapp2 config
 app_config = {
-  'webapp2_extras.sessions': {
-    'cookie_name': '_simpleauth_sess',
-    'secret_key': constants.SESSION_KEY
-  },
-  'webapp2_extras.auth': {
-    'user_attributes': [],
-    'user_model': WhysaurusUser
-  }
+    'webapp2_extras.sessions': {
+        'cookie_name': '_simpleauth_sess',
+        'secret_key': constants.SESSION_KEY
+    },
+    'webapp2_extras.auth': {
+        'user_attributes': [],
+        'user_model': WhysaurusUser
+    }
 }
 
 app = WSGIApplication(routes=routes, config=app_config, debug=True)
 
-def main():
-  run_wsgi_app(app)
-
 if __name__ == '__main__':
-  main()
+    run_wsgi_app(app)
--- constants.py
+++ constants.py
@@ -1,17 +1,16 @@
 import os
 
 # This is a session secret key used by webapp2 framework.
-# Get 'a random and long string' from here: 
+# Get 'a random and long string' from here:
 # http://clsc.net/tools/random-string-generator.php
 # or execute this from a python shell: import os; os.urandom(64)
 SESSION_KEY = "\x13f\xdaE\x86\xa7#\xe2\xd5\xd7\x04\x84\x18<\xe9\xbc9o\xa4\xb9?\x19\xe9Ie\xfd\xec~\xee\x03\xae]\xf7g\xcc^6\xa7i\xe0\x1a\xfa\xa3\xf7<\rk]'\xde6\xa8\xf5,u\x00Q;\x101\xd9\xc42\xd2"
 
 
-
 DEV = os.environ['SERVER_SOFTWARE'].startswith('Development')
 
 if DEV:
-    FACEBOOK_APP_ID  = '460138394028773'
+    FACEBOOK_APP_ID = '460138394028773'
     FACEBOOK_APP_SECRET = 'ae64953822e937d9fed9daaa959390e9'
     FACEBOOK_CHANNEL_URL = '//localhost:8081/channel.html'
     GOOGLE_APP_ID = '730565153520.apps.googleusercontent.com'
@@ -41,22 +40,22 @@ TWITTER_CONSUMER_SECRET = 'VrFcSJpKa2vcPFOJUbQWeiL5oRPbq3Iiu3XFw2t1zoo'
 
 # config that summarizes the above
 AUTH_CONFIG = {
-  # OAuth 2.0 providers
-  'google'      : (GOOGLE_APP_ID, GOOGLE_APP_SECRET,
-                  'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email'),
-  'facebook'    : (FACEBOOK_APP_ID, FACEBOOK_APP_SECRET,
-                  'user_about_me email'),
-  'windows_live': (WL_CLIENT_ID, WL_CLIENT_SECRET,
-                  'wl.signin'),
+    # OAuth 2.0 providers
+    'google': (GOOGLE_APP_ID, GOOGLE_APP_SECRET,
+               'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email'),
+    'facebook': (FACEBOOK_APP_ID, FACEBOOK_APP_SECRET,
+                 'user_about_me email'),
+    'windows_live': (WL_CLIENT_ID, WL_CLIENT_SECRET,
+                     'wl.signin'),
 
-  # OAuth 1.0 providers don't have scopes
-  'twitter'     : (TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET),
-  'linkedin'    : (LINKEDIN_CONSUMER_KEY, LINKEDIN_CONSUMER_SECRET),
+    # OAuth 1.0 providers don't have scopes
+    'twitter': (TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET),
+    'linkedin': (LINKEDIN_CONSUMER_KEY, LINKEDIN_CONSUMER_SECRET),
 
-  # OpenID doesn't need any key/secret
+    # OpenID doesn't need any key/secret
 }
 
 SCORETHRESHOLDS = {
-  'green' : 2,
-  'red' : -2
+    'green': 2,
+    'red': -2
 }

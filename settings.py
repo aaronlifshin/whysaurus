b574ce82a16b5a45206348567cc45721e90a6089
aaronlifshin <aaronlifshin@gmail.com>
2013-12-27 23:30:20 -0500

    Cached django loaders added. Small text change to Aaron profile

--- settings.py
+++ settings.py
@@ -24,3 +24,10 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.request',
 )
+
+TEMPLATE_LOADERS = (
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
+)

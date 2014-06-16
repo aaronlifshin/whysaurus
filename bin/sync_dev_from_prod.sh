34e29d23d3b11efc307c522305872d4aca7055fd
Colin Curtin <colin.t.curtin@gmail.com>
2014-06-15 17:20:37 -0700

    Accept GAE_ROOT env var, or try to find it

--- bin/sync_dev_from_prod.sh
+++ bin/sync_dev_from_prod.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+if [ -z "${GAE_ROOT}" ]; then
+	dir=$(which dev_appserver.py)
+	if [ -n "${dir}" ]; then
+		GAE_ROOT=$(dirname $dir)
+	fi
+fi
+
+if [ -z "${GAE_ROOT}" ]; then
+	echo "GAE_ROOT is not set, and couldn't find Google App Engine SDK"
+	exit 1
+fi
+
 today=$(date "+%Y%m%d")
-appcfg.py download_data --url=http://whysaurus.appspot.com/_ah/remote_api --filename=/tmp/whysaurus-prod-$today
-appcfg.py upload_data --url=http://localhost:8081/_ah/remote_api --filename=/tmp/whysaurus-prod-$today
+$GAE_ROOT/appcfg.py download_data --url=http://whysaurus.appspot.com/_ah/remote_api --filename=/tmp/whysaurus-prod-$today
+$GAE_ROOT/appcfg.py upload_data --url=http://localhost:8081/_ah/remote_api --filename=/tmp/whysaurus-prod-$today

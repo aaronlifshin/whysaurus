acbd9d3a4854ad830a055ef36ccee5722ea10fdf
Colin Curtin <colin.t.curtin@gmail.com>
2014-06-15 17:20:15 -0700

    Fix css compile script

--- bin/compilelessc.sh
+++ bin/compilelessc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-lessc less/bootstrap.less static/css/bootstrap.css
-lessc less/responsive.less static/css/bootstrap-responsive.css
-lessc less/new.less static/css/new.css
+# gem install less
+lessc less/bootstrap.less > static/css/bootstrap.css
+lessc less/responsive.less > static/css/bootstrap-responsive.css
+lessc less/new.less > static/css/new.css

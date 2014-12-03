80d645eb1e022891f29fc9300f8329d73081944d
Marian Reynov <mreynov@gmail.com>
2014-12-02 20:38:32 -0800

    cleaning up areauser model

--- models/areauser.py
+++ models/areauser.py
@@ -7,19 +7,14 @@ import logging
 class AreaUser(ndb.Model):
     userKey = ndb.StringProperty(required=True)
     privateArea = ndb.StringProperty(required=True)
-    uniqueAreaUserString = ndb.StringProperty()
-    
-    def __init__(self, *args, **kwargs):
-        super(AreaUser, self).__init__(*args, **kwargs)
-        self.uniqueAreaUserString = 'AreaUser.uniqueAreaUserString.%s:%s' % (self.userKey, self.privateArea)  
     
     def putUnique(self):
-        success = Unique.create(self.uniqueAreaUserString)
+        success = Unique.create('AreaUser.uniqueAreaUserString.%s:%s' % (self.userKey, self.privateArea))
         if success:    
             self.put()
 
     def deleteRelationship(self):
-        success, existing = Unique.create_multi([self.uniqueAreaUserString])        
+        success, existing = Unique.create_multi(['AreaUser.uniqueAreaUserString.%s:%s' % (self.userKey, self.privateArea)])        
         Unique.delete_multi(existing)
         self.key.delete();
         

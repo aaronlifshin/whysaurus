6c6916299cdba93793767104a6b7efc072f9e433
justlikeUNIBLAB <joshua.frankel@gmail.com>
2018-02-02 14:57:02 -0500

    fixed a bug on the contact form and cleaned it up a bit

--- handlers/contactsend.py
+++ handlers/contactsend.py
@@ -1,6 +1,7 @@
 import os
 import constants
 import datetime
+from models.timezones import PST
 
 from google.appengine.ext.webapp import template
 from google.appengine.api import mail
@@ -12,11 +13,10 @@ class ContactSend(AuthHandler):
         mail.send_mail(
             sender='community@whysaurus.com',
             to='community@whysaurus.com',
-            subject='WHYSAURUS CONTACT RE: ' + self.request.get('subject') +' date: '+ PST.convert(datetime.datetime.now()),
-            body='From: ' + self.request.get('name') + '\n' + self.request.get('message'),
+            subject='WHYSAURUS CONTACT RE: ' + self.request.get('subject') +'  || DATE: '+ str(PST.convert(datetime.datetime.now())),
+            body='From: ' + self.request.get('email') + '\n\n' + self.request.get('message'),
             reply_to=self.request.get('email')
         )
-
         template_values = {
             'message': "Thank you for your message.",
         }      

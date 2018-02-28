08a278ce008ea00603f53ea3c266752821bb4b43
Travis Vachon <travis.vachon@gmail.com>
2018-02-28 14:11:32 -0800

    First version of title text component

--- static/js/test/TitleText.test.jsx
+++ static/js/test/TitleText.test.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { TitleText } from '../ys/components/TitleText'
 import renderer from 'react-test-renderer'
+import { Form } from 'react-form';
 
 
 test('TitleText component renders', () => {
   const component = renderer.create(
-    <TitleText></TitleText>,
+    <Form>
+      {formApi => (<TitleText></TitleText>)}
+    </Form>
   )
 
   let tree = component.toJSON()

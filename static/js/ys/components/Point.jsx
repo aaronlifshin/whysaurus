aa1ddc72c8f06fd634255b7c66dc2ecb4947c2d5
Joshua Frankel <joshua.frankel@gmail.com>
2018-03-19 15:22:50 -0400

    more menu copy and icons cleaner

--- static/js/ys/components/Point.jsx
+++ static/js/ys/components/Point.jsx
@@ -621,10 +621,10 @@ class PointCardComponent extends React.Component {
       <ul id="" className="dropdown-menu dropdown-menu-with-caret" role="menu" aria-labelledby="dropdownMenu">
         <div className="dropdown-caret"><div className="caret-outer"></div><div className="caret-inner"></div></div>
         <li><a onClick={this.handleClickEdit} className="" ><span className="iconWithStat fas fa-pencil-alt"></span>Edit Claim</a></li>
-        <li><a onClick={this.handleClickNoProp} target="_blank" href={"/pointCard/" + this.point.url}><span className="iconWithStat fas fa-external-link-alt"></span>Open in a new tab</a></li>
+        <li><a onClick={this.handleClickNoProp} target="_blank" href={"/pointCard/" + this.point.url}><span className="iconWithStat fas fa-external-link-alt"></span>Open in new tab</a></li>
         { this.currentUserIsAdmin() && <li className="dropdownMenuCategory">Admin</li>}
         { this.currentUserIsAdmin() && this.hasParent() && <li><a onClick={this.handleClickUnlink}><span className="iconWithStat fa fa-unlink"></span>Unlink</a></li>  }
-        { this.currentUserIsAdmin() && <li><a onClick={this.handleClickDelete}><span className="iconWithStat fa fa-trash"></span>Delete</a></li>  }
+        { this.currentUserIsAdmin() && <li><a onClick={this.handleClickDelete}><span className="iconWithStat far fa-trash-alt"></span>Delete</a></li>  }
       </ul>
     </span>
   }

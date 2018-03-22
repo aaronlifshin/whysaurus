8e69247b76888f4c7e247fb261956ce1d7ca2a60
Travis Vachon <travis.vachon@gmail.com>
2018-03-22 13:12:44 -0700

    Initial work on image uploads
    Currently using the filestack CDN, which doesn't really meet our
    needs, but this is a proof of concept of the transformation logic at least.

--- static/js/ys/config.js
+++ static/js/ys/config.js
@@ -1,8 +1,11 @@
 export default {
   filestack: {
-    key: "AinmHvEQdOt6M2iFVrYowz"
+    //    key: "AinmHvEQdOt6M2iFVrYowz"
+    key: "AAKWcX68GSwmsGzZp7ZiFz"
   },
-
+  cdn: {
+    baseURL: "http://cdn.filestackcontent.com/"
+  },
   relevanceThreshold: 35,
   newPointsPageSize: 10
 }

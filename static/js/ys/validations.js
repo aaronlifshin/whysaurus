85f6e337d26a6c96f979df8837bfe50ebdcaa2c1
Joshua Frankel <joshua.frankel@gmail.com>
2018-04-19 17:13:43 -0400

    commenting out the warning on numChars <1 for now, bc our logic for disabling the publish button is taking care of it, and our WIP logic for displaying the feedback area is making this warning flash in a distracting way

--- static/js/ys/validations.js
+++ static/js/ys/validations.js
@@ -2,7 +2,10 @@ export const titleMaxCharacterCount = 200;
 
 export function validateTitle(title){
   if (!title || title.trim() === '') {
-    return "This claim doesn't seem to have any words.";
+    // Disabling this warning for now bc our logic for disabling the publish button is taking care of it, 
+    // and our WIP logic for displaying the feedback area is making this flash in a distracting way -JF 
+    //   return "This claim doesn't seem to have any words.";
+    return "";
   } else if (title.length > titleMaxCharacterCount){
     return "This is over the character limit. Try dividing your idea into multiple claims. Remember: brevity is the soul of wit!";
   } else {

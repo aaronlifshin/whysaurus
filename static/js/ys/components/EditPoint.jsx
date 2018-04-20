40a95c08670215b019ba499a06868254f177315e
Joshua Frankel <joshua.frankel@gmail.com>
2018-04-20 16:22:04 -0400

    height of edit claim text form being set based on number of characters in the claim currently

--- static/js/ys/components/EditPoint.jsx
+++ static/js/ys/components/EditPoint.jsx
@@ -8,7 +8,9 @@ import Spinner from './Spinner'
 import CloseLinkX from './Point'
 
 
-const EditTitleForm = ({point, onSubmit, onClick}) => {
+
+const EditTitleForm = ({point, onSubmit, onClick, heightClass}) => {
+  let titleTextClasses = `titleTextField ${heightClass}`
   return (
     <Form onSubmit={onSubmit}
           defaultValues={{title: point.title}}
@@ -16,7 +18,7 @@ const EditTitleForm = ({point, onSubmit, onClick}) => {
       { ({submitForm, values: {title}, validationFailures}) => (
         <form onSubmit={submitForm} id="form1" className="editPointTextForm">
           <div className="claimCreationFormFieldBlock">
-            <TitleText onClick={onClick} id="editPointTextField" className="titleTextField" onSubmit={submitForm}/>
+            <TitleText onClick={onClick} id="editPointTextField" className={titleTextClasses} onSubmit={submitForm}/>
           </div>
           <div className="claimCreationFormButtonBlock">
             <button onClick={onClick} disabled={(!title || (title == "") || (title.length > validations.titleMaxCharacterCount) ) || (validationFailures > 0)} className="buttonUX2 createClaimFormButton" type="submit">Save</button>
@@ -47,6 +49,20 @@ class EditPointComponent extends React.Component {
     }).then(null, (err) => this.setState({saving: false}))
   }
 
+  // TODO: this algorithm is a sketch, might not be ideal 
+  getEditTextFormHeightClass(title){
+    if (title.length > 160 )
+      return "sixLines";
+    if (title.length > 130 )
+      return "fiveLines";
+    if (title.length > 100 )
+      return "fourLines";
+    if (title.length > 65 )
+      return "threeLines";    
+    else 
+      return "twoLines";
+  }  
+  
   render(){
     let editClaimTextClasses = `claimEditArea pointCardPaddingH editClaimText`
     if (this.state.saving) {
@@ -59,7 +75,7 @@ class EditPointComponent extends React.Component {
           <span className="heading">Edit Claim Text</span>
           <span className="editAreaClose"><a onClick={this.props.onCancel}><CloseLinkX/></a></span>
         </span>
-        <EditTitleForm onClick={this.handleClickNoProp} onSubmit={this.handleClickSave} point={this.point} countedValue={this.point.title}/>
+        <EditTitleForm onClick={this.handleClickNoProp} onSubmit={this.handleClickSave} point={this.point} countedValue={this.point.title} heightClass={this.getEditTextFormHeightClass(this.point.title)}/>
 
 
       </div>;

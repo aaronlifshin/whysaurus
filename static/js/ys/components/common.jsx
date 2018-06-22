1e9188be385bd782e410c5c8572dd7c64977e04d
Gordon P. Smith <gordon.smith@meeteor.com>
2018-06-22 16:08:11 -0400

    Added formatter functions for TimeAgo on PointCard and Comments

--- static/js/ys/components/common.jsx
+++ static/js/ys/components/common.jsx
@@ -1,3 +1,16 @@
 import React from 'react'
 
 export const CloseLinkX  = () => <span>&#xd7;</span>
+
+export const timeAgoFormatter = (value, unit, suffix, date) => {
+  let unitMap = {"second": "s", "minute": "m", "hour": "h", "day": "d", "week": "w", "mon": "mo", "year": "y"}
+  return `${value}${unitMap[unit]}`
+}
+
+export const timeAgoTitle = (date) => {
+  let i = date.indexOf("T")
+  if (i != -1)
+    return date.slice(0, i)
+  else
+    return date
+}
